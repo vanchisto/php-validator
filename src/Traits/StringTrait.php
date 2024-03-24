88dec3ec8365b8407c2fe783877a5a3fc310b03e
Ivan Chistyakov <vanchisto@gmail.com>
2024-03-24 23:46:11 +0300

    Add Validator.php

---
+++ src/Traits/StringTrait.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Vanchisto\Validator\Traits;
+
+trait StringTrait
+{
+    /**
+     * @param mixed $value
+     * @return bool
+     */
+    public static function isStr(mixed $value): bool
+    {
+        return is_string($value);
+    }
+
+    /**
+     * @param mixed $value
+     * @return bool
+     */
+    public static function isStrLen(mixed $value): bool
+    {
+        return static::isStr($value) && strlen($value);
+    }
+}
---
+++ src/Validator.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Vanchisto\Validator;
+
+use Vanchisto\Validator\Traits\StringTrait;
+use Vanchisto\Validator\Traits\ArrayTrait;
+
+class Validator
+{
+    use StringTrait;
+    use ArrayTrait;
+}
